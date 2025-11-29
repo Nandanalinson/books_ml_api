ecff66cd22ea816dc4141f48db3e9240cb14290b
Nandanalinson <nandanalinson7@gmail.com>
2025-11-29 14:42:10 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+books = pd.read_csv("Books.csv")
+ratings = pd.read_csv("Ratings.csv")
+users = pd.read_csv("Users.csv")
+
+print(books.shape)
+print(ratings.shape)
+print(users.shape)
